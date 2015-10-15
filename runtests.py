6f2f4f62b2553d005ba911e9c801a0e04f1bb829
Falk Schuetzenmeister <falk.schuetzenmeister@tnc.org>
2015-10-15 11:05:11 -0700

    patch runtests for Django 1.6.5 and freeze to 1.8.3

--- runtests.py
+++ runtests.py
@@ -4,11 +4,18 @@ import sys
 
 import django
 from django.conf import settings
+
+# put this here for now as long as we support Django 1.6.5
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
+
 from django.test.utils import get_runner
 
 if __name__ == "__main__":
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
-    django.setup()
+    try:
+        django.setup()
+    except AttributeError:
+        pass
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
     failures = test_runner.run_tests(["tests"])
