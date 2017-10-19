ebed0157fc850afc8276af0036948a057f865914
Falk Schuetzenmeister <falk.schuetzenmeister@tnc.org>
2017-10-19 13:34:27 -0700

    fail gracefully if sqlite drver not required by python 3

--- runtests.py
+++ runtests.py
@@ -4,16 +4,20 @@ import sys
 import django
 from django.conf import settings
 
+
 # put this here for now as long as we support Django 1.6.5
 os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
+
 from django.test.utils import get_runner
+
 
 tests = 'tests'
 try:
     tests = sys.argv[1]
 except IndexError:
     pass
+
 
 if __name__ == "__main__":
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
