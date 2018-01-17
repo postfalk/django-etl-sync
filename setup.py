74a06e5f3f2cbaf508af4cdcc1b66cdf75e341b3
Falk Schuetzenmeister <falk.schuetzenmeister@tnc.org>
2018-01-16 16:57:50 -0800

    Update setup.py for new release

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-etl-sync',
-    version='0.3.2',
+    version='0.3.3',
     packages=['etl_sync'],
     include_package_data=True,
     license='BSD License',
     description='Django ETL, derives rules from models, creates relations.',
     url='https://github.com/postfalk/django-etl-sync.git',
-    download_url='https://github.com/postfalk/django-etl-sync/tarball/0.3.2',
+    download_url='https://github.com/postfalk/django-etl-sync/tarball/0.3.3',
     author='Falk Schuetzenmeister',
     author_email='schuetzenmeister@berkeley.edu',
     install_requires=['future', 'six', 'backports.csv'],
@@ -26,12 +26,13 @@ setup(
         'Framework :: Django :: 1.7',
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
+        'Framework :: Django :: 2.0',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content']
 )
